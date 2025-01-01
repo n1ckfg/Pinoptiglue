4067a7ad6aed4f95b60bed22a6e595420715304b
Nick Fox-Gieg <nick@fox-gieg.com>
2025-01-01 15:11:55 -0500

    test

--- src/ofApp.h
+++ src/ofApp.h
@@ -72,7 +72,7 @@ class ofApp : public ofBaseApp {
 		ofFbo screenFbo;
 		ofPixels screenPixels;
 		
-		//bool newFrameToProcess;
+		bool newFrameToProcess;
 
 		ofx::Video::IPVideoGrabber camIp;
 		
@@ -81,28 +81,12 @@ class ofApp : public ofBaseApp {
 		
 		ofFbo targetBlendFbo;
 		ofPixels targetBlendPixels;
-		ofImage piCamTarget, camUsbTarget, mjpegInTarget;
-		string comboBlend;
-		ofBlendMode comboBlendMode;
+		ofImage mjpegInTarget;
 		
 		ofImage gray;
 		ofImage grayThumbnail;
 		int syncVideoQuality; // 5 best to 1 worst, default 3 medium
 		bool videoColor;
-
-		// for more camera settings, see:
-		// https://github.com/orgicus/ofxCvPiCam/blob/master/example-ofxCvPiCam-allSettings/src/testApp.cpp
-
-	    int camRotation;        
-	    int camShutterSpeed; // 0 to 330000 in microseconds, default 0
-	    int camSharpness; // -100 to 100, default 0
-	    int camContrast; // -100 to 100, default 0
-	    int camBrightness; // 0 to 100, default 50
-		int camIso; // 100 to 800, default 300
-		int camExposureCompensation; // -10 to 10, default 0;
-
-		// 0 off, 1 auto, 2 night, 3 night preview, 4 backlight, 5 spotlight, 6 sports, 7, snow, 8 beach, 9 very long, 10 fixed fps, 11 antishake, 12 fireworks, 13 max
-		int camExposureMode; // 0 to 13, default 0
 
 		//string oscAddress;
 		int thresholdValue; // default 127
